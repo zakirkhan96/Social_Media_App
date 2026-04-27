9ed738c4101458cac5b1eaf49cd6b71bfcf2d207
~LoStIlluSIon <zk548473@gmail.com>
2026-04-27 17:51:16 +0500

    Add profile page in navbar

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from accounts import views
-from posts import views
+from . import views
 from .views import register
 
 urlpatterns = [
