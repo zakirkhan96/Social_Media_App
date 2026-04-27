fe956ee30f522de217f06bd2a32e84ff44140d31
~LoStIlluSIon <zk548473@gmail.com>
2026-04-28 00:16:56 +0500

    Update PFP to show also in feed/home page.

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profiles/', default='profiles/default.png')
+    image = models.ImageField(upload_to='media/profiles/', default='profiles/default.png')
     bio = models.TextField(blank=True)
 
     def __str__(self):
